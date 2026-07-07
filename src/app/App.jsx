09ded9c4405fb36bfcce7fb2ebbbfe23a2aa7991
Emirchik <emirmarash@gmail.com>
2026-07-06 23:57:03 -0600

    v1 header

---
+++ src/app/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Header from '../components/Header.jsx'
+function App() {
+
+  return (
+    <>
+      <BrowserRouter>
+          <Header/>
+          <Routes>
+            
+          </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
